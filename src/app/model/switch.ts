4893620801a9c5d36f88ca0d924f81febe3e0179
grippom04 <103483194+grippom04@users.noreply.github.com>
2023-05-09 22:06:48 -0300

    Cambio total en el aspecto visual

--- src/app/model/switch.ts
+++ src/app/model/switch.ts
@@ -1,8 +1,8 @@
 
 export const Switch = {
   
-    //url : 'https://portfoliobackend-5ne1.onrender.com'
-     url :'http://localhost:8080'
+    url : 'https://portfoliobackend-5ne1.onrender.com'
+    //url :'http://localhost:8080'
 
 }
 
